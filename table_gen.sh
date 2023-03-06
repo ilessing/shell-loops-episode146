3114b6d3995d9e3caf807b52d95adeb353579264
Ian Lessing <ilessing@library.ucsb.edu>
2023-03-05 20:26:52 -0800

    handle entry of non-numbers

--- table_gen.sh
+++ table_gen.sh
@@ -5,6 +5,12 @@ echo "I can produce multiplication tables for you. "
 echo " What multiplication table would you like to see?"
 read -p "Enter a number: " users_number
 
+# error out if user does not provide a whole number
+re='^[0-9]+$'
+if ! [[ $users_number =~ $re ]] ; then
+   echo "error: Not a whole number" >&2; exit 1
+fi
+
 echo "You want a multiplication table for ${users_number}.  Okay.  Can do."
 
 for iteration in {1..10}
