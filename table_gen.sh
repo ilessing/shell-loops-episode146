a8e83b5935c592037eec30ba96fbd0f5d703e549
Ian Lessing <ilessing@library.ucsb.edu>
2023-03-05 22:27:11 -0800

    handle second argument

--- table_gen.sh
+++ table_gen.sh
@@ -1,6 +1,30 @@
 #!/usr/bin/env bash
 
+# usage:
+# table_gen [number [max number]]
+#   generates a multiplication table for given [number]
+#   defaults to ten rows.
+#   if users provides a second argument it will be used 
+#   as the number of rows in the table.
+
 users_number=$1
+users_max=$2
+
+regularexp='^[0-9]+$'
+
+# test if a second argument was provided
+if [[ -z $users_max ]]
+then
+  # default range max to ten when none is provided
+  range_max=10
+
+  # error out if user provided a non-number as second argument
+elif ! [[ $users_max =~ $regularexp ]]
+  then 
+    echo "error: Not a whole number" >&2; exit 1
+  else
+  range_max=$users_max
+fi
 
 if [[ -z $users_number ]]
 then
@@ -11,14 +35,13 @@ then
 fi
 
 # error out if user does not provide a whole number
-re='^[0-9]+$'
-if ! [[ $users_number =~ $re ]] ; then
+if ! [[ $users_number =~ $regularexp ]] ; then
    echo "error: Not a whole number" >&2; exit 1
 fi
 
 echo "You want a multiplication table for ${users_number}.  Okay.  Can do."
 
-for iteration in {1..10}
+for iteration in $(seq 1 $range_max)
 do
 	echo -n "${iteration} * ${users_number} = "
 	echo "$iteration * $users_number" |bc
